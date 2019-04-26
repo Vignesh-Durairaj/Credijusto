c9043f6344bcd37d1322503ebadcaa754d43861b
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-04-27 01:50:15 +0530

    Added XML to JSON parsing logic

--- src/main/java/com/credijusto/fxrates/ExchangeRateService.java
+++ src/main/java/com/credijusto/fxrates/ExchangeRateService.java
@@ -1,11 +1,13 @@
 package com.credijusto.fxrates;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.credijusto.utils.RateProviderUtil;
@@ -14,7 +16,7 @@ import com.credijusto.utils.RateProviderUtil;
 public class ExchangeRateService {
 
 	@GET
-	public Response getFXRates() throws IOException {
+	public Response getFXRates() throws IOException, JSONException, ParseException {
 		RateProviderUtil util = new RateProviderUtil();
 		JSONObject fxRatesJson = util.getFxRatesJson();
 		return Response.status(200).entity(fxRatesJson).build();
--- src/main/java/com/credijusto/utils/RateProviderUtil.java
+++ src/main/java/com/credijusto/utils/RateProviderUtil.java
@@ -3,55 +3,64 @@ package com.credijusto.utils;
 import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.credijusto.model.FXRateModel;
 
+@SuppressWarnings("deprecation")
 public class RateProviderUtil {
 
-	@SuppressWarnings("deprecation")
 	public FXRateModel getFixerRate() throws IOException {
-		String fixerURL = "http://data.fixer.io/api/latest?access_key=1019eae0726ebd47e19ba52e5056852e&base=USD&base=EUR&symbols=USD,MXN";
-		String urlString = IOUtils.toString(new URL(fixerURL));
-		JSONObject jsonObj = new JSONObject(urlString);
+		String urlString = "http://data.fixer.io/api/latest?access_key=1019eae0726ebd47e19ba52e5056852e&base=USD&base=EUR&symbols=USD,MXN";
+		String fixerURL = IOUtils.toString(new URL(urlString));
+		JSONObject jsonObj = new JSONObject(fixerURL);
 		JSONObject ratesObject = (JSONObject)jsonObj.get("rates");
 		Double eurToMxn = (Double)ratesObject.get("MXN");
 		Double eurToUSD = (Double)ratesObject.get("USD");
 		Integer timeStamp = (Integer)jsonObj.get("timestamp");
 		
 		FXRateModel fxRateModel = new FXRateModel();
-		fxRateModel.setProviderName("FIXER");
+		fxRateModel.setProviderName("Fixer");
 		fxRateModel.setFxRate(eurToMxn / eurToUSD);
 		fxRateModel.setLastUpdatedDate(new Date(timeStamp * 1000L));
 		
 		return fxRateModel;
 	}
 	
-	public FXRateModel getBanxicoRate() {
+	public FXRateModel getBanxicoRate() throws IOException, ParseException {
+
+		String urlString = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/oportuno?token=4412e21fd8f0467a7219f8a44be4c3ed8a283894184da634911bac32f082bdb9";
+		String banxicoURL = IOUtils.toString(new URL(urlString));
+		JSONObject banxicoJson = new JSONObject(banxicoURL);
+		JSONObject dato = ((JSONObject)((JSONArray)((JSONObject)((JSONArray)((JSONObject)banxicoJson.get("bmx")).get("series")).get(0)).get("datos")).get(0));
+		
 		FXRateModel fxRateModel = new FXRateModel();
 		fxRateModel.setProviderName("Banxico");
-		fxRateModel.setFxRate(18.928974);
-		fxRateModel.setLastUpdatedDate(new Date());
+		fxRateModel.setFxRate(Double.valueOf((String)dato.get("dato")));
+		fxRateModel.setLastUpdatedDate(new SimpleDateFormat("dd/MM/yyyy").parse((String)dato.get("fecha")));
 		
 		return fxRateModel;
 	}
 	
 	public FXRateModel getDofRate() {
 		FXRateModel fxRateModel = new FXRateModel();
-		fxRateModel.setProviderName("Diario Official de la Federacion");
+		fxRateModel.setProviderName("Diario Official de la Federacion(Hardcoded)");
 		fxRateModel.setFxRate(18.931174);
 		fxRateModel.setLastUpdatedDate(new Date());
 		
 		return fxRateModel;
 	}
 	
-	public List<FXRateModel> getAllFxRates() throws IOException {
+	public List<FXRateModel> getAllFxRates() throws IOException, ParseException {
 		List<FXRateModel> fxRateModels = new ArrayList<>();
 		fxRateModels.add(getDofRate());
 		fxRateModels.add(getFixerRate());
@@ -60,9 +69,10 @@ public class RateProviderUtil {
 		return fxRateModels;
 	}
 	
-	public JSONObject getFxRatesJson() throws IOException {
+	public JSONObject getFxRatesJson() throws IOException, ParseException {
 		
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'");
+		df.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		JSONObject providerJson = new JSONObject(), responseJson = new JSONObject();
 		List<FXRateModel> allFXRates = getAllFxRates();
@@ -77,5 +87,10 @@ public class RateProviderUtil {
 		
 		responseJson.put("rates", providerJson);
 		return responseJson;
+	}
+	
+	public static void main(String[] args) throws  IOException, ParseException {
+		RateProviderUtil util = new RateProviderUtil();
+		System.out.println(util.getFxRatesJson());
 	}
 }
